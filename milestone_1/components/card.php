833032bf4b1e1ffc0f2ca8a45b5f818a0d71fc54
5oN1A <82112240+5oN1A@users.noreply.github.com>
2021-12-01 16:12:08 +0100

    milestone_1

---
+++ milestone_1/components/card.php
@@ -0,0 +1,13 @@
+<?php
+echo"<div class='col'>
+<div class='card'>
+    <img class='card-img-top' src='$img' alt='...'/>
+    <div class='card-body text-center'>
+      <h4 class='card-title py-2'>$title</h4>
+      <div class='card-text'>
+        <h4 class='mb-0'>$author</h4>
+        <h6 class='pb-5'>$year</h6>
+      </div>
+      </div>
+    </div>
+  </div> ";
